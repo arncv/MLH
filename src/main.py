e813147d152104b66595a6a2edc88fb6871a2e30
Arnav <arnavgoel7903@gmail.com>
2023-08-05 18:11:51 +0530

    added data set and rm some bugs

--- src/main.py
+++ src/main.py
@@ -1,14 +1,18 @@
-import webbrowser
+import pandas as pd
 from taipy.gui import Gui, Markdown, notify
 
 # Define your page
 page = Markdown(
 """
+<center>
+<|navbar|lov={[("page1", "Homepage"), ("https://docs.taipy.io/en/latest/manuals/about/", "Taipy Docs"), ("https://docs.taipy.io/en/latest/getting_started/", "Getting Started")]}|>
+</center>
+
 # Financial Planner
 
-Enter your monthly income: <|income|text|on_action=handle_change|>
+Enter your monthly income: <|{income}|input|>
 
-Enter your monthly expenses: <|expenses|text|on_action=handle_change|>
+Enter your monthly expenses: <{expenses}input|>
 
 <|Calculate|button|on_action=on_calculate|>
 
@@ -17,21 +21,12 @@ Your savings: <|savings|text|>
 )
 
 # Define action functions
-def handle_change(state, var_name: str, var_value):
-    if var_name == "income":
-        state.vars.income = float(var_value)
-    elif var_name == "expenses":
-        state.vars.expenses = float(var_value)
-
 def on_calculate(state):
-    income = state.vars.get('income')
-    expenses = state.vars.get('expenses')
-    if income and expenses:
-        savings = calculate_savings(income, expenses)
-        state.vars.savings = f"Your savings are {savings}"
-        notify(state, "success", f"Your savings are {savings}")
-    else:
-        notify(state, "error", "Please enter your income and expenses")
+    income = float(state.vars.get('income', 0))
+    expenses = float(state.vars.get('expenses', 0))
+    savings = calculate_savings(income, expenses)
+    state.vars.savings = f"Your savings are {savings}"
+    notify(state, "success", f"Your savings are {savings}")
 
 # Define helper functions
 def calculate_savings(income, expenses):
